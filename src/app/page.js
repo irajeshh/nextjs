b10df037522358ca537fc07efe65ee148269bc54
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-28 19:20:08 +0530

    HomePage samples added for sync, async

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
-import Counter from "@/app/CounterPage";
-// import Counter from "@/components/Counter";
+'use client';
 
-export default function Home() {
+import { useRouter } from 'next/navigation';
+import * as W from '@/Widgets/Widgets';
+
+export default function HomePage() {
+  // const router = useRouter();
+
+
+
   return (
+    <W.Column>
+      <W.Txt text="Welcome to the Store" isBold={true} fontSize={20} />
+      <W.Linker
+        text="View Products"
+        href="/products"
+        openInNewTab={false}
+      />
+      <W.Linker
+        text="View Counter"
+        href="/counter"
+        openInNewTab={false}
+      />
 
-    <Counter />
+    </W.Column>
   );
 }
