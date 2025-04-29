d5b96a58850e9faa1119c90f908d104533da7ab7
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 07:49:45 +0530

    Loading indicated with text

--- src/app/products/loading.js
+++ src/app/products/loading.js
@@ -3,7 +3,7 @@
 export default function Loading() {
     return (
         <div className="p-4">
-            <h1 className="text-2xl mb-4">Loading Products...</h1>
+            <h1 className="text-2xl mb-4">Loading will take 10 seconds fake delay...</h1>
             <div className="space-y-4">
                 {Array.from({ length: 15 }).map((_, i) => (
                     <div key={i} className="h-12 bg-gray-300 rounded animate-pulse"></div>
