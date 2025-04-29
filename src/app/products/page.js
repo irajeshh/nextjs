5d14e259ec6f7f801595402838123fa323d73340
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 08:47:49 +0530

    Products page fully impelmented with Widgets

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'; // Import Link from Next.js
 import { getAllProducts } from '@/Repos/Products';
 import * as W from '@/Widgets/Widgets';
 
@@ -6,19 +5,27 @@ import * as W from '@/Widgets/Widgets';
 /// and will be shown only after loading the products from repository
 export default async function ProductsPage() {
     const products = await getAllProducts();
-
     return (
-        <div className="max-w-6xl mx-auto p-6">
-            <h1 className="text-3xl font-bold mb-8 text-center">Our Products Page</h1>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <W.Column>
+            <W.Txt
+                text="Our Products For You!"
+                isBold={true}
+                fontSize={48}
+                center={true}
+            />
+            <W.GridView>
                 {products.map(product => (
                     <W.Linker
                         text={product.name}
                         href={`/products/${product.id}`}
                         openInNewTab={false}
                     />
-                ))}
-            </ul>
-        </div>
+                ))
+                }
+
+            </W.GridView>
+        </W.Column>
+
     );
+
 }
