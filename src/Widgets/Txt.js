9de3856e4e7e319a719cec0bc7a21509158e580c
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 08:32:34 +0530

    Txt widget improved

--- src/Widgets/Txt.js
+++ src/Widgets/Txt.js
@@ -1,8 +1,9 @@
-export function Txt({ text, isBold, fontSize }) {
+export function Txt({ text, isBold, fontSize, center }) {
     const fontWeight = isBold ? "font-bold" : "";
     const customStyle = fontSize ? { fontSize: `${fontSize}px` } : {};
+    const textAlign = center ? "text-center" : "";
     return (
-        <div className={`${fontWeight}`} style={customStyle}>
+        <div className={`${fontWeight} ${textAlign}`} style={customStyle}>
             {text}
         </div>
     );
