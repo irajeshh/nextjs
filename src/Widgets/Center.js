195fa0f4c208f80b20d2590c75a913a9be76f40c
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 08:47:05 +0530

    Center widget improved with child

--- src/Widgets/Center.js
+++ src/Widgets/Center.js
@@ -1,10 +1,11 @@
 
 //This puts the given child in the center of the screen
 //But unlike flutter we cannot put [child], instead we need to pass [children] only!
-export function Center({ children }) {
+//so we use child and then use <></> in the UI pages to pass the children via child param
+export function Center({ child }) {
     return (
         <div className="min-h-screen flex items-center justify-center p-6">
-            {children}
+            {child}
         </div>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,30 +4,35 @@ import * as W from '@/Widgets/Widgets';
 
 export default function HomePage() {
   return (
-    <W.Center>
-      <W.Column>
-        <W.Txt
-          text="Welcome to the Store"
-          isBold={true}
-          fontSize={48}
-          className="text-center"
-        />
+    <W.Center
+      child={
+        <>
+          <W.Column>
+            <W.Txt
+              text="Welcome to the Store"
+              isBold={true}
+              fontSize={48}
+              center={true}
+            />
 
-        <div className="flex flex-col items-center gap-6">
-          <W.Linker
-            text="🛒 View Products list"
-            href="/products"
-            openInNewTab={false}
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-          />
-          <W.Linker
-            text="🔢 View Counter"
-            href="/counter"
-            openInNewTab={false}
-            className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
-          />
-        </div>
-      </W.Column>
-    </W.Center>
+            <div className="flex flex-col items-center gap-6">
+              <W.Linker
+                text="🛒 View Products list"
+                href="/products"
+                openInNewTab={false}
+                className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+              />
+              <W.Linker
+                text="🔢 View Counter"
+                href="/counter"
+                openInNewTab={false}
+                className="px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+              />
+            </div>
+          </W.Column>
+        </>
+
+      }
+    />
   );
 }
