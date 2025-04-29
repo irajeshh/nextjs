8137221db8283aee8435f44e4f8fb32d29233ca5
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 08:12:43 +0530

    Linker implemented in Products Page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -12,17 +12,11 @@ export default async function ProductsPage() {
             <h1 className="text-3xl font-bold mb-8 text-center">Our Products Page</h1>
             <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {products.map(product => (
-                    <li key={product.id} className="border rounded-lg p-4 shadow hover:shadow-lg transition">
-                        {/* Use Link directly without <a> */}
-                        {/* <Link href={`/products/${product.id}`}>
-                            <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-                        </Link> */}
-                        <W.Linker
-                            text={product.name}
-                            href={`/products/${product.id}`}
-                            openInNewTab={false}
-                        />
-                    </li>
+                    <W.Linker
+                        text={product.name}
+                        href={`/products/${product.id}`}
+                        openInNewTab={false}
+                    />
                 ))}
             </ul>
         </div>
