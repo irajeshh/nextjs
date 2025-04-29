3405b67b9c399ac07b7bb5e516488c6a32f7286b
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-04-29 07:18:10 +0530

    product details added with loading.js

---
+++ src/app/products/[id]/loading.js
@@ -0,0 +1,20 @@
+// src/app/products/[id]/loading.js
+
+export default function Loading() {
+    return (
+        <div className="max-w-2xl mx-auto p-6">
+            <div className="animate-pulse space-y-4">
+                {/* Shimmer Skeleton for Title */}
+                <div className="h-6 bg-gray-300 rounded w-3/4"></div>
+
+                {/* Shimmer Skeleton for Image */}
+                <div className="h-64 bg-gray-300 rounded"></div>
+
+                {/* Shimmer Skeleton for Description */}
+                <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+                <div className="h-4 bg-gray-300 rounded w-4/6"></div>
+                <div className="h-4 bg-gray-300 rounded w-2/3"></div>
+            </div>
+        </div>
+    );
+}
